3b8b2e7e0a9d179767144ce3474f37922c9d24e8
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-11-18 16:16:09 -0500

    circle progress bug fix.

--- src/components/my-circle-progress/my-circle-progress.tsx
+++ src/components/my-circle-progress/my-circle-progress.tsx
@@ -6,30 +6,47 @@ import { Component, Prop, h } from "@stencil/core";
   shadow: true
 })
 export class MyCircleProgress {
-  @Prop() progressAmount: string = "20";
+  @Prop() progressAmount: string = "0";
   @Prop() progressColor: string = "#2ecc71";
 
-  render() {
+render() {
     return (
-      <div
+        <div
         style={{
-          width: `100%`,
-          height: `100%`,
-          display: `flex`,
-          alignItems: `center`,
-          justifyContent: `center`,
-          background: `conic-gradient(${this.progressColor} ${this.progressAmount}%, 0, #ecf0f1 ${(
+            width: `100%`,
+            height: `100%`,
+            display: `flex`,
+            alignItems: `center`,
+            justifyContent: `center`,
+            background: `conic-gradient(${this.progressColor} ${this.progressAmount}%, 0, #ecf0f1 ${(
             100 - parseInt(this.progressAmount)
-          ).toString()}%)`,
-          borderRadius: `50%`
+            ).toString()}%)`,
+            borderRadius: `50%`
         }}
-      >
-        <div>
-          <span>
+        >
+        <div
+            style={{
+            display: `flex`,
+            alignItems: `center`,
+            justifyContent: `center`,
+            backgroundColor: `#fff`,
+            height: `80%`,
+            width: `80%`,
+            borderRadius: `50%`,
+            boxShadow: `0px 0px 7px 0px rgba(0, 0, 0, 0.1)`
+            }}
+        >
+            <span
+            style={{
+                fontFamily: `"Helvetica Neue", Helvetica, Arial, Verdana, sans-serif`,
+                fontSize: `2em`,
+                fontWeight: `lighter`
+            }}
+            >
             {this.progressAmount}
-          </span>
+            </span>
         </div>
-      </div>
+        </div>
     );
-  }
+    }
 }
